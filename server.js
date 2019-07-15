7977956276e4f7e2e48663596a373425d3a4ffc2
Myskiv_Viktor <vmy@e-boks.dk>
2019-07-15 17:06:21 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,56 @@
+
+// This is simple sample how to serve static website and save form data to file with NodeJS
+// Author: programming mentor
+// Usage:
+// 1. Install dependencies: npm i express body-parser
+// 2. Run: server node server.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, './')));
+
+app.post('/login', (req, res) => {
+  const fs = require('fs');
+  fs.appendFile('./logins.txt', JSON.stringify(req.body) + '\n', function(err) {
+    if (err) {
+      res.status(500).send('Server error');
+      return console.log(err);
+    }
+    console.log('Data saved: ' + JSON.stringify(req.body));
+    res.send('Data saved');
+  });
+});
+
+console.log(
+  'Server is running on',
+  process.env.PORT || 3000,
+  process.env.IP || '0.0.0.0'
+);
+
+app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0');
+
+
+document.querySelector('.login-form button[type=submit]')
+    .addEventListener('click', login);
+
+function login(e) {
+    e.preventDefault();
+    fetch('login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({
+                name: document.querySelector('.login-form input[name=name]').value,
+                phone: document.querySelector('.login-form input[name=phone]').value
+            })
+        })
+        .then(_ => document.querySelector('.login-form').reset());
+}
+
+
